4415ff4c80236714fcbdae0a58b99d20ddd884e7
oksanabuzh <95624459+oksanabuzh@users.noreply.github.com>
2026-01-29 11:09:31 +0100

    Compute CWM/FD; add trait coverage checks
    Rework trait preprocessing and FD pipeline: load interspecific traits into trait_data and align species order with species composition; compute per-plot trait coverage and write to data/for_data_checks/trait_coverage_per_plot.csv; drop insufficiently covered trait (aeren_percent). Prepare traits for FD by pivoting categorical columns (aerenchyma, AMF_type, peat_association, PFT, lifespan) into dummy/wide format with NA propagation. Run FD::functcomp (CWM) and FD::dbFD (functional diversity) using sp_compos and the modified trait matrix, and save outputs to data/processed_data/CWM_FunctCompos.csv and data/processed_data/Functional_Diversity.csv. Also include various small fixes: ensure rowname/column conversions, handle NA propagation when creating dummies, simplify summarise grouping, and update variable names for clarity.

--- prepare_data/0.2_calculate_trait_CWM_and_functional_diversity.R
+++ prepare_data/0.2_calculate_trait_CWM_and_functional_diversity.R
@@ -7,91 +7,128 @@ library(tidyverse)
 sp_compos <- read_csv("data/processed_data/species_composition.csv") %>% 
   column_to_rownames("plot_ID") 
 
-traits <- read_csv("data/processed_data/interspecific_traits.csv") %>% 
+trait_data <- read_csv("data/processed_data/interspecific_traits.csv") 
+
+# Prepare trait matrix ----
+# make sure species names are same order as in sp_compos
+traits <- sp_compos %>% 
+  pivot_longer(everything(),names_to = "species", values_to = "cover") %>%
+  select(-cover) %>%
+  distinct() %>% 
+  left_join(trait_data,
+            by = "species") %>% 
   column_to_rownames("species")
 
 
 # Check NAs in traits
-
 summary(traits)
 
 traits %>% 
   filter(is.na(aerenchyma))
 
-# Trait coverage: how much each trait covers in each plot out of total community cover? -------------------------
+# Trait coverage check: how much each trait covers in each plot out of total community cover? -------------------------
 # for each species and plot_ID in sp_compos, check % of cover that each trait covers
-traits_coverage  <- sp_compos %>%
+traits_coverage  <- sp_compos %>% rownames_to_column("plot_ID") %>%
   pivot_longer(-plot_ID, names_to = "species", values_to = "cover") %>%
   filter(cover > 0) %>%
   left_join(traits %>% rownames_to_column("species"), by = "species") %>%
 # for all traits if NA ~ NA, if not NA ~ column "cover"  
   mutate(across(-c(plot_ID, species, cover), 
                 ~ ifelse(is.na(.x), NA, cover))) %>%
-  group_by(plot_ID) %>%
 # for each trait, sum cover where trait is not NA / total cover * 100
   summarise(across(-c(species, cover), 
-                   ~ sum(.x, na.rm = TRUE)/sum(cover) * 100)) %>%
-  ungroup() 
+                   ~ sum(.x, na.rm = TRUE)/sum(cover) * 100),
+            .by=plot_ID) 
+
+
+write_csv(traits_coverage,  
+          "data/for_data_checks/trait_coverage_per_plot.csv")
+
+# Which traits are not sufficiently covered? ----
+# less than 60% coverage 
+traits_coverage %>%
+  pivot_longer(-plot_ID, names_to = "trait", values_to = "coverage") %>%
+  filter(coverage < 60) %>%
+   arrange(trait, coverage) %>% 
+  print(n=Inf)
+
+## ->  Remove insufficient trait data ----
+# Based on the trait coverage results, remove traits with insufficient data coverage across the study plots
+
+traits <- traits %>% 
+  select(-aeren_percent)
+
+
+# Calculate CWM and Functional diversity -------------------------
 
 ## 1) CWM for functional traits ----
 dim(sp_compos)
 dim(traits)
 
 FuncComp <- FD::functcomp(traits, as.matrix(sp_compos), 
-                          CWM.type = "all", 
-                          bin.num=c(  #  bin.num - indicates binary traits to be treated as continuous  
-                            "raunkiaer_phanerophyte", "lifeform_tree.shrub",
-                            "lifeform_herbPoli", "lifeform_herbMono")) 
+                          CWM.type = "all")
+                      #    bin.num=c("")  #  bin.num - indicates binary traits to be treated as continuous
 
 FuncComp
 
 dim(FuncComp)
 
 write_csv(as.data.frame(FuncComp) %>% 
-            rownames_to_column("Plot") %>% 
-            separate(Plot, into = c("PlotNo", "Subplot", "Month"), sep = "_",
-                     remove = TRUE),
-          "data/processed_data/CWM_FunctCompos_1m2.csv")
+            rownames_to_column("plot_ID"), 
+          "data/processed_data/CWM_FunctCompos.csv")
 
 
 ## 2) Functional diversity ----
 
 # When calculating functional diversity, FD::dbFD Error occurs when mixed categorical and numeric traits
 # thus, convert categorical traits to wide format for each category
-Func_groups_modif <- Func_groups %>% 
-  select(-starts_with("raunkiaer_"), # correlate strongly with lifespan
-         -starts_with("lifeform_"),  # correlate strongly with lifespan
-         # exclude all disturb indic for calculating functional diversity
-         -Disturbance.Frequency,-Disturbance.Severity,  
-         -Mowing.Frequency, -Grazing.Pressure, -Soil.Disturbance) %>%
-  rownames_to_column("Taxon") %>%
-  # for column status, create wide a format 
-  mutate(present = 1) %>%
-  pivot_wider(names_from = status, values_from=present, values_fill = 0,
-              names_prefix = "status_") %>%
-  mutate(across(starts_with("status_"), ~ replace(.x, status_NA==1, NA))) %>%
-  select(-status_NA) %>% 
-  # for column functional_group, create a wide format 
-  mutate(present = 1) %>%
-  pivot_wider(names_from = functional_group, values_from=present, values_fill = 0,
-              names_prefix = "FunGr_") %>%
-  column_to_rownames("Taxon")
 
-str(Func_groups_modif)
+traits_modif <- traits %>% rownames_to_column("species") %>% 
+  # for column aerenchyma, create a wide format 
+  mutate(present = 1) %>%
+  pivot_wider(names_from = aerenchyma, values_from=present, values_fill = 0,
+              names_prefix = "Aerenchyma_") %>% 
+  mutate(across(starts_with("Aerenchyma_"), ~ replace(.x, Aerenchyma_NA==1, NA))) %>%
+  select(-Aerenchyma_NA) %>% 
+# for column AMF_type, create a wide format 
+  mutate(present = 1) %>%
+  pivot_wider(names_from = AMF_type, values_from=present, values_fill = 0,
+              names_prefix = "AMF_") %>% 
+  mutate(across(starts_with("AMF_"), ~ replace(.x, AMF_NA==1, NA))) %>%
+  select(-AMF_NA) %>% 
+# for column peat_association, create a wide format 
+  mutate(present = 1) %>%
+  pivot_wider(names_from = peat_association, values_from=present, values_fill = 0,
+              names_prefix = "Peat.Sp_") %>% 
+  mutate(across(starts_with("Peat.Sp_"), ~ replace(.x, Peat.Sp_NA==1, NA))) %>%
+  select(-Peat.Sp_NA) %>% 
+# for column PFT, create a wide format 
+  mutate(present = 1) %>%
+  pivot_wider(names_from = PFT, values_from=present, values_fill = 0,
+              names_prefix = "PFT_") %>% 
+  mutate(across(starts_with("PFT_"), ~ replace(.x, PFT_NA==1, NA))) %>%
+  select(-PFT_NA) %>% 
+# for column lifespan, create a wide format 
+  mutate(present = 1) %>%
+  pivot_wider(names_from = lifespan, values_from=present, values_fill = 0,
+              names_prefix = "LifeSpan_") %>% 
+  mutate(across(starts_with("LifeSpan_"), ~ replace(.x, LifeSpan_NA==1, NA))) %>%
+  select(-LifeSpan_NA) %>% 
+  column_to_rownames("species")
+  
+str(traits_modif)
 
-FuncDiv <- FD::dbFD(Func_groups_modif, as.matrix(species_data), 
-                    calc.CWM = F,
-                    corr = c("cailliez")) 
-FuncDiv
+
+FuncDiv <- FD::dbFD(traits_modif, as.matrix(sp_compos), 
+              calc.CWM = F,
+         corr = c("cailliez"))
 
 str(FuncDiv)
 
 FuncDiv <- as.data.frame(FuncDiv) %>% 
-  rownames_to_column("Plot") %>%
-  select(-nbsp, -qual.FRic, -sing.sp) %>% 
-  separate(Plot, into = c("PlotNo", "Subplot", "Month"), sep = "_",
-           remove = TRUE)
+  rownames_to_column("Plot_ID") %>%
+  select(-nbsp, -qual.FRic, -sing.sp) 
 
 
 # save Functional diversity indices
-write_csv(FuncDiv, "data/processed_data/Functional_Diversity_1m2.csv")
+write_csv(FuncDiv, "data/processed_data/Functional_Diversity.csv")
